948540db1fec5ebc83af03bebdd444db4c1c6c3e
Samuel Razali <samuelrazali@gmail.com>
2025-04-22 22:06:28 +1000

    add console log to POST webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,12 +2,15 @@ import client from "@/app/lib/redis";
 import FetchNotion from "@/app/lib/notionFetch";
 // import { NextRequest } from "next/server";
 
-export async function POST() {
+export async function POST(req: Request) {
   // TODO: validate
   //   const secret = req.headers.get("x-notion-secret");
   //   if (secret !== process.env.NOTION_WEBHOOK_SECRET) {
   //     return new NextResponse("Unauthorized", { status: 401 });
   //   }
+  const body = await req.json();
+  console.log("Received webhook:", body);
+  
   try {
     const result = await FetchNotion();
     await client.set("portfolioData", JSON.stringify(result));
