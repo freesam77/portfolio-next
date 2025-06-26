ab5ccb164508c5752e80e1c22da58dbfa4035185
Samuel Razali <samuelrazali@gmail.com>
2025-06-27 00:54:44 +1000

    refactor: change divs to sections for semantic HTML structure and improve accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,22 +13,22 @@ export default function Home() {
             <AnimatedBG />
             <NavigationBar />
             <main className="pt-[100px] md:pt-0 mx-auto w-[80%] max-w-[950px]">
-                <div id="about" className="flex flex-col justify-center min-h-screen mb-[100px]">
+                <section id="about" className="flex flex-col justify-center min-h-screen mb-[100px]">
                     <About />
                     <Likes />
-                </div>
-                <div id="skillset" className="min-h-screen mb-[120px]">
-                    <h1 className="mb-10">Skillset</h1>
+                </section>
+                <section id="skillset" className="min-h-screen mb-[120px]">
+                    <h2 className="mb-10">Skillset</h2>
                     <Skillset />
-                </div>
-                <div id="projects" className="min-h-screen mb-[100px]">
-                    <h1 className="mb-10">Projects</h1>
+                </section>
+                <section id="projects" className="min-h-screen mb-[100px]">
+                    <h2 className="mb-10">Projects</h2>
                     <Projects />
-                </div>
+                </section>
             </main>
-            <div id="contact">
+            <section id="contact">
                 <Contact />
-            </div>
+            </section>
         </div>
     );
 }
