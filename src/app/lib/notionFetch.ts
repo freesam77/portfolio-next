0152b1a3a15894fbf7d2d2a4ab2b5c682648116b
Samuel Razali <samuelrazali@gmail.com>
2025-06-28 17:32:01 +1000

    refactor: modularize Notion data fetching by separating helpers and property processing

--- src/app/lib/notionFetch.ts
+++ src/app/lib/notionFetch.ts
@@ -1,60 +1,6 @@
 "use server";
 
-import { Client } from "@notionhq/client";
-import {
-  PageObjectResponse,
-  PartialPageObjectResponse,
-  PartialDatabaseObjectResponse,
-  DatabaseObjectResponse,
-} from "@notionhq/client/build/src/api-endpoints";
-
-import notionDatabaseProcessor from "@/app/lib/notionDatabaseProcessor";
-import notionPageToHTML from "./notionPageToHTML";
-
-// Initialize Notion client
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
-
-// Type guard
-const isPageObjectResponse = (
-  item:
-    | PageObjectResponse
-    | PartialPageObjectResponse
-    | PartialDatabaseObjectResponse
-    | DatabaseObjectResponse,
-): item is PageObjectResponse => {
-  return "properties" in item;
-};
-
-const pageFetch = async (database_id: string, sectionName: string) => {
-  const result = await notionPageToHTML(database_id, notion);
-  return { [sectionName]: result };
-};
-
-const databaseFetch = async (
-  database_id: string,
-  sectionName: string,
-  pageContentAsDescription: boolean = false,
-) => {
-  const response = await notion.databases.query({ database_id });
-
-  const result = await Promise.all(
-    response.results.map(async (objectResponse) => {
-      if (!isPageObjectResponse(objectResponse)) {
-        throw new Error("Response type is not page object");
-      }
-      const processedData = notionDatabaseProcessor(objectResponse.properties);
-      if (pageContentAsDescription) {
-        const description = await notionPageToHTML(objectResponse.id, notion);
-        Object.assign(processedData, {
-          description,
-        });
-      }
-      return processedData;
-    }),
-  );
-
-  return { [sectionName]: result };
-};
+import { pageFetch, databaseFetch } from "./notionFetchHelpers";
 
 export const fetchLandingPage = async () => {
   return await pageFetch(process.env.NOTION_LANDING_PAGE_DB!, "landingPage");
