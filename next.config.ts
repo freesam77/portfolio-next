30f1fdd1568bc5512b9471eb7809699cda0bf1df
Sam <samuelrazali@gmail.com>
2025-11-26 18:30:43 +1100

    Restore whitelist to use the old links for images

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,50 @@ const nextConfig: NextConfig = {
 	images: {
 		// Add formats for better optimization
 		formats: ['image/webp', 'image/avif'],
-		// Allow Notion file uploads from AWS S3
-		domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
+		// Configure allowed image sources using modern remotePatterns
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'i.imgur.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'imgur.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'placehold.co',
+			},
+			{
+				protocol: 'https',
+				hostname: 'github.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn2.iconfinder.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn0.iconfinder.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn.svglogos.dev',
+			},
+			{
+				protocol: 'https',
+				hostname: 'upload.wikimedia.org',
+			},
+			{
+				protocol: 'https',
+				hostname: 'svgrepo.com',
+				pathname: '/show/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+			},
+		],
 	},
 	// Optimize for static generation
 	output: 'standalone',
